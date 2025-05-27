b5173262d51d3b6158df2b2531454f9298f2582d
MFaizAdira <148429683+MFaizAdira@users.noreply.github.com>
2025-05-27 10:10:23 +0700

    input create movie

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,20 +3,27 @@
 @section('content')
     <h1>Latest Movies</h1>
 
+    @if (session('success'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ session('success') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endif
+
     <div class="row">
         {{-- looping data movies --}}
         @foreach ($movies as $movie)
-        <div class="col-lg-6">
-            <div class="card mb-3">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="{{ $movie->cover_image }}" class="img-fluid rounded-start" alt="...">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $movie->title }}</h5>
-                            <p class="card-text">{{ Str::words($movie->synopsis, 20, '...') }}</p>
-                            <a href="" class="btn btn-success">Read More</a>
+            <div class="col-lg-6">
+                <div class="card mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="{{ asset('storage/' . $movie->cover_image) }}" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $movie->title }}</h5>
+                                <p class="card-text">{{ Str::words($movie->synopsis, 20, '...') }}</p>
+                                <a href="/movie/{{ $movie->id }}/{{ $movie->slug }}" class="btn btn-success">Read More</a>
                             </div>
                         </div>
                     </div>
@@ -24,6 +31,6 @@
             </div>
         @endforeach
         {{-- end loop --}}
-        {{ $movies->links(); }}
+        {{ $movies->links() }}
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
 
-Route::get('/', [MovieController::class, 'homePage']);
+Route::get('/',[MovieController::class, 'homePage']);
+
+Route::get('movie/{id}/{slug}',[MovieController::class, 'detail']);
+
+Route::get('create_movie', [MovieController::class, 'create'])->name('createMovie');
+
+Route::post('/movies', [MovieController::class, 'store'])->name('movies.store');
+
